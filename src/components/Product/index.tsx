3369fb5d33b42fb3c7f14e82152134c966c101bb
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-29 17:29:39 -0300

    refactor Product component to use props for dynamic rendering of product details

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -1,27 +1,22 @@
-import hamburgerImage from '../../assets/hamburger.png';
-import styles from './product.module.css';
+import hamburgerImage from "../../assets/hamburger.png";
+import { TProductProps } from "../../interfaces/product.interface";
+import styles from "./product.module.css";
 
-export function Product(): JSX.Element {
-  
+export function Product({ item }: Readonly<TProductProps>): JSX.Element {
+  const { title, description, price } = item;
   return (
     <div className={styles.product}>
       <img
         src={hamburgerImage}
         alt="hamburguer"
-        className={styles.product__image} />
-      <h3 className={styles.product__title}>
-        Hamburguer 2 queijos
-      </h3>
-      <p className={styles.product__description}>
-      Pão - Carne 180g - Mussarela - Cheedar
-      </p>
-      <h2 className={styles.product__price}>
-        R$ 36,00
-      </h2>
-      <button      
-        className={`${styles.product__button} button--primary button`}>
+        className={styles.product__image}
+      />
+      <h3 className={styles.product__title}>{title}</h3>
+      <p className={styles.product__description}>{description}</p>
+      <h2 className={styles.product__price}>{price}</h2>
+      <button className={`${styles.product__button} button--primary button`}>
         add ao carrinho
       </button>
     </div>
-  )
+  );
 }
