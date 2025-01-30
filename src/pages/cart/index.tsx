9fd84490e764cd7a84ca95973ceb0a0669630220
WashingtonDDS <washington.DDS@hotmail.com>
2025-01-30 12:31:24 -0300

    calculate and display total price in Cart component with formatting

--- src/pages/cart/index.tsx
+++ src/pages/cart/index.tsx
@@ -2,10 +2,21 @@ import styles from "./cart.module.css";
 import { ProductInCart } from "../../components/ProductInCart";
 import { useLoaderData } from "react-router-dom";
 import { TProductInCart } from "../../interfaces/productCart.interface";
+import { useEffect, useState } from "react";
+import { formatPrice } from "../../utils/formatPrice";
 
 export function Cart(): JSX.Element {
   const productsInCart = useLoaderData() as TProductInCart[];
-
+  const [total, setTotal] = useState<string>("R$ 0");
+  const calcTotal = () => {
+    const total = productsInCart.reduce(
+      (acc: number, item: TProductInCart) =>
+        (acc += item.quantity * item.price),
+      0
+    );
+    setTotal(formatPrice(total));
+  };
+  useEffect(() => calcTotal(), []);
   return (
     <main className="main__container container">
       <section className={styles.products}>
@@ -15,7 +26,7 @@ export function Cart(): JSX.Element {
             <ProductInCart key={item.id} item={item} />
           ))}
         </div>
-        <h1 className={styles.products__total}>Total: R$ 100,00</h1>
+        <h1 className={styles.products__total}>Total: {total}</h1>
       </section>
     </main>
   );
